b3d5b693d27d4254e376957847919770469532be
Vadym Linets <vadym.linets@computools.com>
2023-03-20 22:28:12 +0200

    _mf

--- server/middleware/recover-middleware.go
+++ server/middleware/recover-middleware.go
@@ -3,12 +3,13 @@ package middleware
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog/log"
 )
 
 func RecoverMiddleware() echo.MiddlewareFunc {
 	return middleware.RecoverWithConfig(middleware.RecoverConfig{
 		LogErrorFunc: func(c echo.Context, err error, stack []byte) error {
-			// log.Alert(errb.NewError("RecoverMiddleware").WithMessageAndError("Server fatal message", err))
+			log.Error().Str("function", "RecoverMiddleware").Err(err).Bytes("stack", stack).Msg("Server fatal error")
 			return nil
 		},
 	})
